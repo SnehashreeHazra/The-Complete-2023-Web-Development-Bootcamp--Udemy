0b7e0f2943c7f0856e4c4d161f07934ab8d0e6ae
Snehashree Hazra <snehashreehazra783@gmail.com>
2023-10-14 21:22:59 +0530

    Update loveScore-project.js

--- JavaScript/loveScore-project.js
+++ JavaScript/loveScore-project.js
@@ -2,4 +2,9 @@ prompt("What is your name?");
 prompt("What is their name?");
 var loveScore = Math.random() * 100;
 loveScore = Math.floor(loveScore) + 1;
-alert("Your love score is " + loveScore + "%");
+
+if (loveScore > 70) {
+    alert("Your love score is " + loveScore + "%" + " and You love each other like kyne loves kyne.");
+} else {
+    alert("Your love score is " + loveScore + "%");
+}
